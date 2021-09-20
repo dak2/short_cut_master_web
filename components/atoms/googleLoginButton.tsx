bcdfa106bcab811543769c1bcdd87aab014fa842
dak2 <dawt2h@gmail.com>
2021-09-20 16:15:07 +0900

    request user info to server

--- components/atoms/googleLoginButton.tsx
+++ components/atoms/googleLoginButton.tsx
@@ -1,7 +1,21 @@
 import GoogleLogin from "react-google-login";
+import axios from "axios";
 
-const responseGoogle = (response: any) => {
-  console.log(response);
+const responseGoogle = async (response: any) => {
+  console.log("response", response);
+  try {
+    /* TODO : 開発環境と本番環境で適切なオリジンを指定する */
+    const res = await axios.post("http://localhost:3000/", {
+      withCredentials: true,
+      token: response.tokenId,
+    });
+    /* TODO : ログイン後の画面遷移 */
+    /* TODO : ログイン情報の保存 */
+    console.log(res);
+  } catch (error) {
+    /* TODO : ログイン失敗のダイアログ表示 */
+    console.log(error);
+  }
 };
 
 const GoogleLoginButton = () => {
